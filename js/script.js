38ed1599feb6c4d8337ae409fd492eb9f9c771c8
Alicja Kaszuba <kaszubaalicja.96@gmail.com>
2021-01-19 19:58:44 +0100

    Code for game in module 5

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Computer move
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
@@ -8,12 +9,13 @@ if(randomNumber == 1){
   computerMove = 'kamień';
 } else if(randomNumber == 2){
     computerMove = 'papier';
-} else {
+} else if(randomNumber == 3){
     computerMove = 'nożyce';
 }
 
 printMessage('Mój ruch to: ' + computerMove);
 
+// Player move
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
@@ -24,8 +26,33 @@ if(playerInput == '1'){
   playerMove = 'kamień';
 } else if(playerInput == '2'){
     playerMove = 'papier';
-} else {
+} else if(playerInput == '3'){
     playerMove = 'nożyce';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+// Game result
+
+if(computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Ja wygrywam!');
+  } else if(computerMove == 'nożyce' && playerMove == 'kamień'){
+      printMessage('Ty wygrywasz!');
+  } else if(computerMove == 'papier' && playerMove == 'kamień'){
+      printMessage('Ja wygrywam!');
+  } else if(computerMove == 'kamień' && playerMove == 'papier'){
+      printMessage('Ty wygrywasz!');
+  } else if(computerMove == 'nożyce' && playerMove == 'papier'){
+      printMessage('Ja wygrywam!');
+  } else if(computerMove == 'papier' && playerMove == 'nożyce'){
+      printMessage('Ty wygrywasz!');
+  } else if(computerMove == 'kamień' && playerMove == 'kamień'){
+      printMessage('Remis.');
+  } else if(computerMove == 'papier' && playerMove == 'papier'){
+      printMessage('Remis.');
+  } else if(computerMove == 'nożyce' && playerMove == 'nożyce'){
+      printMessage('Remis.');
+  } else if(playerMove == 'nieznany ruch'){
+      printMessage('Nie da się rozstrzygnąć gry. Musisz wybrać odpowiedni ruch!')
+  }
+
